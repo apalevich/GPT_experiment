0c603173b12d14a88d3fb1db261a8eaf834b753d
Artyom Palevich <hello@apalevich.ru>
2023-04-15 14:46:32 +0400

    Add chatHistory so that bot remembers the context

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 import { openai, userInterface } from "./helpers.js"
 
+const chatHistory = [];
+
 const inputHandler = async input => {
+    const query = {
+        role: 'user',
+        content: input
+    };
+    chatHistory.push(query);
+
     const res = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
-        messages: [{
-            role: 'user',
-            content: input
-        }]
+        messages: chatHistory
     })
+    
+    chatHistory.push(res.data.choices[0].message)
     console.log(res.data.choices[0].message.content)
     userInterface.prompt()
 };
